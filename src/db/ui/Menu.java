f5775f0a3d1d651a2814ad2454b82a240ebf8c7d
Alvaro de la Fuente <a.fuente9@usp.ceu.es>
2021-03-17 20:33:29 +0100

    Creation of menu

---
+++ src/db/ui/Menu.java
@@ -0,0 +1,16 @@
+package db.ui;
+
+import java.io.*;
+import java.util.*;
+
+import db.interfaces.Interface;
+import db.jdbc.JDBCManagment;
+
+public class Menu {
+	private static Interface inter = new JDBCManagment();
+	private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) {
+		
+	}
+}
