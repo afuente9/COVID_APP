0d7e02ae3d57c096695520d1a0ef7921a95bad37
anchu09 <d.anchuela@usp.ceu.es>
2021-04-16 20:04:35 +0200

    finishing gui 1

--- src/db/GUI/SearchPatientController.java
+++ src/db/GUI/SearchPatientController.java
@@ -1,8 +1,11 @@
 package db.GUI;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import db.pojos.Patient;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 public class SearchPatientController implements Initializable {
@@ -19,6 +23,8 @@ public class SearchPatientController implements Initializable {
  
     @FXML
     private ChoiceBox<String> SearchOptions;
+    @FXML
+    private TableView<?> tablePatients;
 
     @FXML
     void BackFromModify(ActionEvent event) {
@@ -41,6 +47,12 @@ public class SearchPatientController implements Initializable {
     	loadData();
     	SearchOptions.setValue("Select an option");
     	numberofpatients.setText("There are "+/*lista.gettotalnumberofpatients+*/"patients");
+    	List <Patient> allpatients= Main.getInter().searchPatientByName("");
+    	//TODO IMPRIMIR LISTA EN LA TABLA
+    	//tablePatients.set
+    	
+    			
+    	
     	
     }
     private void loadData() {
