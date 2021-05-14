2febf336c6f32198d474c0ca9e7896d2370d4dab
anchu09 <d.anchuela@usp.ceu.es>
2021-05-14 19:08:32 +0200

    aa

---
+++ src/db/GUI/SignGovController.java
@@ -0,0 +1,31 @@
+package db.GUI;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SignGovController {
+
+    @FXML
+    private TextField NameTextField;
+
+    @FXML
+    private TextField CifTextField;
+
+    @FXML
+    private TextField NameTextField2;
+
+    @FXML
+    private TextField CifTextField2;
+
+    @FXML
+    void OnAcceptLabClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnCancelLabClick(ActionEvent event) {
+
+    }
+}
