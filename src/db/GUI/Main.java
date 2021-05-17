ecb5a2a8076dd0e1d01a8e393998a6a680ad7d9c
anchu09 <d.anchuela@usp.ceu.es>
2021-05-17 18:43:11 +0200

    starting loggin

--- src/db/GUI/Main.java
+++ src/db/GUI/Main.java
@@ -4,23 +4,35 @@ import java.io.InputStreamReader;
 import java.time.format.DateTimeFormatter;
 
 import db.interfaces.Cov_Manager;
+import db.interfaces.UserManager;
 import db.jdbc.JDBCManagment;
+import db.jpa.JPAUserManagment;
+import db.pojos.users.User;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 public class Main extends Application{
 	private static Cov_Manager inter = new JDBCManagment();
 	private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	
+	private static UserManager userman = new JPAUserManagment();
 	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 	
+	public static UserManager getUserman() {
+		return userman;
+	}
+	public static void setUserman(UserManager userman) {
+		Main.userman = userman;
+	} 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			getInter().connect();
+			getUserman().connect();
 
 			Pane root = (Pane)FXMLLoader.load(getClass().getResource("MainMenuView.fxml"));
 			Scene scene = new Scene(root,700,700);
--- src/db/GUI/SignUpDoctorController.java
+++ src/db/GUI/SignUpDoctorController.java
@@ -1,22 +1,78 @@
 package db.GUI;
 
 import java.net.URL;
+import java.security.MessageDigest;
 import java.sql.Date;
 import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import db.interfaces.UserManager;
+import db.jpa.JPAUserManagment;
 import db.pojos.*;
+import db.pojos.users.Role;
+import db.pojos.users.User;
 
 public class SignUpDoctorController implements Initializable {
+
+  
+
+    @FXML
+    private Button createUserbutton;
+
+  
+
+    
+
+    @FXML
+    private Label Sexlabel;
+
+    @FXML
+    private Label passwordlabel;
+
+    @FXML
+    private Label EmailLabel;
+
+  
+
+    @FXML
+    private Label repeatLabel;
+
+    @FXML
+    private PasswordField RepeatPassword;
+  
+
+    @FXML
+    private Label specialityLabel;
+
+    @FXML
+    private Label collabel;
+
+    @FXML
+    private Label bdlabel;
+
+
+	User u;
+
+    @FXML
+    private Label namelabel;
+
 
     @FXML
     private TextField DateTextField;
 
+    @FXML
+    private Label hoslabel;
+
+  
     @FXML
     private TextField NameTextField;
 
@@ -30,7 +86,7 @@ public class SignUpDoctorController implements Initializable {
     private TextField EmaDcotorText;
 
     @FXML
-    private TextField pasDoctorTxtF;
+    private PasswordField pasDoctorTxtF;
     @FXML
     private TextField SexTextField;
     @FXML
@@ -57,12 +113,77 @@ public class SignUpDoctorController implements Initializable {
     	byte[] image=null;
     	Doctor d_new= new Doctor(name,spetiality,date,colnum,hospital,sex,image);
     	//añadir doctor a la lista de doctores
-    	Main.getInter().addDoctor(d_new);
+    	Main.getInter().addDoctorUser(d_new, u);
+
     	
 
 
     	Stage stage = (Stage) this.NameTextField.getScene().getWindow();
 		stage.close();    	
+    }
+    @FXML
+    void oncreateuser(ActionEvent event) {
+    	String doctoremail = EmaDcotorText.getText();
+    	String doctorPassword =     pasDoctorTxtF.getText();
+    	if(!doctoremail.equals("")&&!doctorPassword.equals("")&&!RepeatPassword.getText().equals("")) {
+			if(doctorPassword.equals(RepeatPassword.getText())) {
+	    		
+	        	
+	        	
+	        	NameTextField.setDisable(false);
+	        	DateTextField.setDisable(false);
+	        	ColNumTextField.setDisable(false);
+	        	NameTextField.setDisable(false);
+	        	SpetialityTextField.setDisable(false);
+	        	HospitalTextField.setDisable(false);
+	        	namelabel.setDisable(false);
+	        	bdlabel.setDisable(false);
+	        	collabel.setDisable(false);
+	        	specialityLabel.setDisable(false);
+	        	hoslabel.setDisable(false);
+	        	SexTextField.setDisable(false);
+	        	Sexlabel.setDisable(false);
+	        	EmaDcotorText.setDisable(true);
+	        	repeatLabel.setDisable(true);
+	        	passwordlabel.setDisable(true);
+	        	EmailLabel.setDisable(true);
+	        	EmaDcotorText.setDisable(true);
+	        	pasDoctorTxtF.setDisable(true);
+	        	RepeatPassword.setDisable(true);
+	        	createUserbutton.setDisable(true);
+	        	Role role = Main.getUserman().getRole(1);
+	        	
+	        	System.out.println(role.getName());
+	        	System.out.println(doctoremail);
+	        	if (Main.getUserman().checkEmail(doctoremail)) {
+					JOptionPane.showMessageDialog(null, "Email already used, try to log in");
+
+				}else {
+					try {
+					
+					MessageDigest md = MessageDigest.getInstance("MD5");
+					md.update(doctorPassword.getBytes());
+					byte[] hash = md.digest();
+					User u = new User(doctoremail, hash, role);
+					Main.getUserman().newUser(u);
+					
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+				}
+	        	
+	         
+
+	    		
+	    	}else {
+				JOptionPane.showMessageDialog(null, "Passwords are not equal");
+
+	    	}
+    	}else {
+			JOptionPane.showMessageDialog(null, "Empty fields");
+
+    	
+    }
     }
 
     @FXML
